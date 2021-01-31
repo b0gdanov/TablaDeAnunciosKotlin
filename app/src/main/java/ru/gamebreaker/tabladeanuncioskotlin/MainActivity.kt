acd90cdb502fb7be529360d9881a268289a27577
Anton Bogdanov <bogdanovtoha@gmail.com>
2021-01-31 22:42:10 +0300

    Урок 2 добавили плагин kotlin-android-extensions доделали выдвижное меню

--- app/src/main/java/ru/gamebreaker/tabladeanuncioskotlin/MainActivity.kt
+++ app/src/main/java/ru/gamebreaker/tabladeanuncioskotlin/MainActivity.kt
@@ -2,10 +2,88 @@ package ru.gamebreaker.tabladeanuncioskotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_content.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
     }
+
+    private fun init(){
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+
+            R.id.id_my_ads ->{
+
+                val text = "Нажата кнопка " + getString(R.string.ad_my_ads)
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+
+            }
+            R.id.id_car ->{
+
+                val text = "Нажата кнопка " + getString(R.string.ad_car)
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+
+            }
+            R.id.id_pc ->{
+
+                val text = "Нажата кнопка " + getString(R.string.ad_pc)
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+
+            }
+            R.id.id_smart ->{
+
+                val text = "Нажата кнопка " + getString(R.string.ad_smartphone)
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+
+            }
+            R.id.id_dm ->{
+
+                val text = "Нажата кнопка " + getString(R.string.ad_dm)
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+
+            }
+            R.id.id_sign_up ->{
+
+                val text = "Нажата кнопка " + getString(R.string.ac_sign_up)
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+
+            }
+            R.id.id_sign_in ->{
+
+                val text = "Нажата кнопка " + getString(R.string.ac_sign_in)
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+
+            }
+            R.id.id_sign_out ->{
+
+                val text = "Нажата кнопка " + getString(R.string.ac_sign_out)
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+
+            }
+
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+
+    }
+
 }
