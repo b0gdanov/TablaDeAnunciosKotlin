aaa2bb3898cc0232de3d27e3f26e2bcfa1997f38
LionGames <31481210+LionGames@users.noreply.github.com>
2021-04-12 23:56:31 +0300

    Урок 26. функция для уменьшения размера картинок.

--- app/src/main/java/ru/gamebreaker/tabladeanuncioskotlin/utils/ImageManager.kt
+++ app/src/main/java/ru/gamebreaker/tabladeanuncioskotlin/utils/ImageManager.kt
@@ -1,10 +1,16 @@
 package ru.gamebreaker.tabladeanuncioskotlin.utils
 
 import android.graphics.BitmapFactory
+import android.util.Log
 import androidx.exifinterface.media.ExifInterface
 import java.io.File
 
 object ImageManager {
+
+    const val MAX_IMAGE_SIZE = 1000
+    const val WIDTH = 0
+    const val HEIGHT = 1
+
     fun getImageSize(uri : String) : List<Int>{
         val options = BitmapFactory.Options().apply {
             inJustDecodeBounds = true
@@ -26,5 +32,28 @@ object ImageManager {
             0
         }
         return rotation
+    }
+
+    fun imageResize(uris : List<String>){
+        val tempList = ArrayList<List<Int>>()
+        for (n in uris.indices){
+            val size = getImageSize(uris[n])
+            Log.d("MyLog", "Width : ${size[WIDTH]} Height : ${size[HEIGHT]}")
+            val imageRatio = size[WIDTH].toFloat() / size[HEIGHT].toFloat()
+            if (imageRatio >1){
+                if (size[WIDTH] > MAX_IMAGE_SIZE){
+                    tempList.add(listOf(MAX_IMAGE_SIZE, (MAX_IMAGE_SIZE / imageRatio).toInt()))
+                } else {
+                    tempList.add(listOf(size[WIDTH], size[HEIGHT]))
+                }
+            } else {
+                if (size[HEIGHT] > MAX_IMAGE_SIZE){
+                    tempList.add(listOf((MAX_IMAGE_SIZE * imageRatio).toInt(), MAX_IMAGE_SIZE))
+                } else {
+                    tempList.add(listOf(size[WIDTH], size[HEIGHT]))
+                }
+            }
+            Log.d("MyLog", "Width : ${tempList[n][WIDTH]} Height : ${tempList[n][HEIGHT]}")
+        }
     }
 }
