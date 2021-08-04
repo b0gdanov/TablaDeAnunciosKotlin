0f4373322daba0955f2a24a507980995de16bfa1
Anton Bogdanov <bogdanovtoha@gmail.com>
2021-08-04 21:41:46 +0300

    Урок 60. Новая версия Pix Image Picker Часть 3

--- app/src/main/java/ru/gamebreaker/tabladeanuncioskotlin/utils/ImageManager.kt
+++ app/src/main/java/ru/gamebreaker/tabladeanuncioskotlin/utils/ImageManager.kt
@@ -23,39 +23,14 @@ object ImageManager {
 
     fun getImageSize(uri : Uri, act: Activity) : List<Int>{
         val inStream = act.contentResolver.openInputStream(uri)
-        val fTemp = File(act.cacheDir, "temp.tmp")
-        if (inStream != null) {
-            fTemp.copyInStreamToFile(inStream)
-        }
         val options = BitmapFactory.Options().apply {
             inJustDecodeBounds = true
         }
-        BitmapFactory.decodeFile(fTemp.path, options)
-        return if (imageRotation(fTemp) == 90)
-            listOf(options.outHeight, options.outWidth)
-        else listOf(options.outWidth, options.outHeight)
-    }
-
-    private fun File.copyInStreamToFile(inStream: InputStream) {
-        this.outputStream().use {
-            out -> inStream.copyTo(out)
-        }
-    }
-
-    private fun imageRotation(imageFile: File) : Int{
-        val rotation : Int
-        val exif = ExifInterface(imageFile.absolutePath)
-        val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
-        rotation = if (orientation == ExifInterface.ORIENTATION_ROTATE_90 || orientation == ExifInterface.ORIENTATION_ROTATE_270){
-            90
-        } else {
-            0
-        }
-        return rotation
+        BitmapFactory.decodeStream(inStream, null, options)
+        return listOf(options.outWidth, options.outHeight)
     }
 
     fun chooseScaleType(im : ImageView, bitmap : Bitmap){
-
         if (bitmap.width > bitmap.height){
             im.scaleType = ImageView.ScaleType.CENTER_CROP
         } else {
