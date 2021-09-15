0955d3fbe01e21d9a6485b334ce0b095986987bf
Anton Bogdanov <bogdanovtoha@gmail.com>
2021-09-16 01:03:23 +0300

    Урок 68. Заполнение экрана просмотра объявлений

--- app/src/main/java/ru/gamebreaker/tabladeanuncioskotlin/act/DescriptionActivity.kt
+++ app/src/main/java/ru/gamebreaker/tabladeanuncioskotlin/act/DescriptionActivity.kt
@@ -32,7 +32,29 @@ class DescriptionActivity : AppCompatActivity() {
 
     private fun getIntentFromMainAct(){
         val ad = intent.getSerializableExtra("AD") as Ad
+        updateUI(ad)
+    }
+
+    private fun updateUI(ad: Ad) {
         fillImageArray(ad)
+        fillTextViews(ad)
+    }
+
+    private fun fillTextViews(ad: Ad) = with(binding){
+        tvTitle.text = ad.title
+        tvDescription.text = ad.description
+        tvFractionD.text = ad.fraction
+        tvHeroNameD.text = ad.heroName
+        //tvEmailD.text = ad.
+        tvTelD.text = ad.tel
+        tvIndexD.text = ad.index
+        checkBoxWithSendD.text = isWithSent(ad.withSend.toBoolean())
+        tvCategoryD.text = ad.category
+        tvPriceD.text = ad.price
+    }
+
+    private fun  isWithSent(withSent: Boolean): String{
+        return if (withSent) "Да" else "Нет"
     }
 
     private fun fillImageArray(ad: Ad){
