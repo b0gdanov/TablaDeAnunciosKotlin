c0786ebef28b6de070c1718b829b2da3452cffbd
Anton Bogdanov <bogdanovtoha@gmail.com>
2022-01-19 20:49:31 +0300

    Урок 95 Встроенные покупки (In app Purchase) #2

--- app/src/main/java/ru/gamebreaker/tabladeanuncioskotlin/utils/BillingManager.kt
+++ app/src/main/java/ru/gamebreaker/tabladeanuncioskotlin/utils/BillingManager.kt
@@ -1,18 +1,47 @@
 package ru.gamebreaker.tabladeanuncioskotlin.utils
 
 import androidx.appcompat.app.AppCompatActivity
-import com.android.billingclient.api.BillingClient
-import com.android.billingclient.api.PurchasesUpdatedListener
+import com.android.billingclient.api.*
 
 class BillingManager(val act: AppCompatActivity) {
     private var billingClient: BillingClient? = null
 
     init {
-
+        setUpBillingClient()
     }
 
     private fun setUpBillingClient(){
         billingClient = BillingClient.newBuilder(act).setListener(getPurchaseListener()).enablePendingPurchases().build()
+    }
+
+    fun startConnection(){
+        billingClient?.startConnection(object : BillingClientStateListener{
+            override fun onBillingServiceDisconnected() {
+
+            }
+
+            override fun onBillingSetupFinished(result: BillingResult) {
+                getItem()
+            }
+
+        })
+    }
+
+    private fun getItem() {
+        val skuList = ArrayList<String>()
+        skuList.add(REMOVE_ADS)
+        val skuDetails = SkuDetailsParams.newBuilder()
+        skuDetails.setSkusList(skuList).setType(BillingClient.SkuType.INAPP)
+        billingClient?.querySkuDetailsAsync(skuDetails.build()) { result, list ->
+            run {
+                if (result.responseCode == BillingClient.BillingResponseCode.OK) {
+                    if (!list.isNullOrEmpty()) {
+                        val billingFlowParams = BillingFlowParams.newBuilder().setSkuDetails(list[0]).build()
+                        billingClient?.launchBillingFlow(act, billingFlowParams)
+                    }
+                }
+            }
+        }
     }
 
     private fun getPurchaseListener(): PurchasesUpdatedListener{
@@ -24,5 +53,9 @@ class BillingManager(val act: AppCompatActivity) {
                 }
             }
         }
+    }
+
+    companion object{
+        const val REMOVE_ADS = "remove_ads"
     }
 }
