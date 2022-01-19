6cf847afb0b9513f89161d5b6cafe4897e513cea
Anton Bogdanov <bogdanovtoha@gmail.com>
2022-01-19 21:11:16 +0300

    Урок 96 Встроенные покупки (In app Purchase) #3

--- app/src/main/java/ru/gamebreaker/tabladeanuncioskotlin/utils/BillingManager.kt
+++ app/src/main/java/ru/gamebreaker/tabladeanuncioskotlin/utils/BillingManager.kt
@@ -1,5 +1,7 @@
 package ru.gamebreaker.tabladeanuncioskotlin.utils
 
+import android.content.Context
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.billingclient.api.*
 
@@ -12,6 +14,13 @@ class BillingManager(val act: AppCompatActivity) {
 
     private fun setUpBillingClient(){
         billingClient = BillingClient.newBuilder(act).setListener(getPurchaseListener()).enablePendingPurchases().build()
+    }
+
+    private fun savePurchase(isPurchase: Boolean){
+        val pref = act.getSharedPreferences(MAIN_PREF, Context.MODE_PRIVATE)
+        val editor = pref.edit()
+        editor.putBoolean(REMOVE_ADS_PREF, isPurchase)
+        editor.apply()
     }
 
     fun startConnection(){
@@ -44,12 +53,29 @@ class BillingManager(val act: AppCompatActivity) {
         }
     }
 
+    private fun nonConsumableItem(purchase: Purchase){
+        if (purchase.purchaseState == Purchase.PurchaseState.PURCHASED){
+            if (!purchase.isAcknowledged){
+                val acParams = AcknowledgePurchaseParams.newBuilder().setPurchaseToken(purchase.purchaseToken).build()
+                billingClient?.acknowledgePurchase(acParams){ result ->
+                    if (result.responseCode == BillingClient.BillingResponseCode.OK){
+                        savePurchase(true)
+                        Toast.makeText(act, "Не удалось совершить покупку!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        savePurchase(false)
+                        Toast.makeText(act, "Спасибо за покупку!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
     private fun getPurchaseListener(): PurchasesUpdatedListener{
         return PurchasesUpdatedListener {
             result, list ->
             run {
                 if (result.responseCode == BillingClient.BillingResponseCode.OK) {
-                    list?.get(0).let {  }
+                    list?.get(0)?.let { nonConsumableItem(it) }
                 }
             }
         }
@@ -57,5 +83,7 @@ class BillingManager(val act: AppCompatActivity) {
 
     companion object{
         const val REMOVE_ADS = "remove_ads"
+        const val MAIN_PREF = "main_pref"
+        const val REMOVE_ADS_PREF = "remove_ads_pref"
     }
 }
